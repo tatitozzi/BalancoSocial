a38a94c660aa241603daf31f29aef3600f3cf2db
Tatiana Tozzi <tatitozzi@hotmail.com>
2024-05-23 12:17:14 -0300

    modificações - acordeon

--- Acordeon - Modificações/script.js
+++ Acordeon - Modificações/script.js
@@ -1,15 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Função para configurar eventos de clique nos botões dos acordeões
     function setupAccordionButton(button, content, secondaryTables) {
         button.onclick = function() {
             let expanded = this.getAttribute('aria-expanded') === 'true';
             this.setAttribute('aria-expanded', !expanded);
-            this.children[0].textContent = expanded ? '+' : '-';
-            if (!expanded) {
-                content.style.maxHeight = content.scrollHeight + 'px';
-            } else {
-                content.style.maxHeight = '0';
-            }
         };
 
         const primaryRows = content.querySelectorAll('#primary-table tbody tr');
@@ -21,7 +14,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // Seleciona todos os acordeões e configura eventos de clique
     const accordions = document.querySelectorAll('.accordion-item');
     accordions.forEach(accordion => {
         const button = accordion.querySelector('.accordion-button');
